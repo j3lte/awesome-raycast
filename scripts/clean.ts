311609e00b525ea788f256bc1a054095ce478ab9
j3lte <jwlagendijk@gmail.com>
2024-08-30 16:30:29 +0200

    Update clean script

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -4,12 +4,12 @@ import updateText from "./utils/update-text.ts";
 const README_FILE = import.meta.resolve("../README.md").replace("file://", "");
 const readme = await Deno.readTextFile(README_FILE);
 
-let updatedReadMe = updateText("SECTIONS", readme, "\n");
-updatedReadMe = updateText("TABLE_OF_CONTENTS", updatedReadMe, "");
-updatedReadMe = updateText("STATISTICS", updatedReadMe, "");
-updatedReadMe = updateText("UPDATETIME", updatedReadMe, "");
+const { updatedText: stage1 } = updateText("SECTIONS", readme, "\n");
+const { updatedText: stage2 } = updateText("TABLE_OF_CONTENTS", stage1, "");
+const { updatedText: stage3 } = updateText("STATISTICS", stage2, "");
+const { updatedText: stageFinal } = updateText("UPDATETIME", stage3, "");
 
-await Deno.writeTextFile(README_FILE, updatedReadMe);
+await Deno.writeTextFile(README_FILE, stageFinal);
 
 const iconsFolder = import.meta.resolve("../icons").replace("file://", "");
 await emptyDir(iconsFolder);
